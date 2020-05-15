9927e9bf5f24da1513ec0fb0029cbfcdcf68882e
Tyler <tylerdburrell@gmail.com>
2020-05-15 09:12:19 -0500

    Intial commit gloomhaven challenge

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-tyler:"+ process.env.DB_PASS +"@cluster0-h3nyl.gcp.mongodb.net/fishingDB", {
+mongoose.connect("mongodb+srv://admin-tyler:"+ process.env.DB_PASS +"@cluster0-h3nyl.gcp.mongodb.net/GloomDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
---
+++ views/scripts/scripts.js
@@ -0,0 +1,39 @@
+var positiveEffectsArray = Array ("Every kill gains 1 experience point",
+"Gold picked up in the scenario is worth its value +1",
+"Player gains 5 gold per door they open up during the scenario",
+"Player may take two battle cards into the scenario",
+"Every healing action gains 1 experience point",
+"If you draw a blessing, gain 5 gold",
+"At the end of the scenario you must take two gold pieces from a teammate",
+"Every time you kill a monster they drop 2 gold",
+"Take battle card from  one teammate");
+
+var negativeEffectsArray = Array ("Start scenario with one lost card chosen by a teammate",
+"Start scenario with two discarded cards at random",
+"Have one item taken from your item selection chosen by your teammate for the scenario",
+"Add one elite ooze to a room of your choice",
+"Player starts scenario with poison & disarm",
+"Player starts scenario with immobilize & wound",
+"Spawn a random elite demon in a room of your choice",
+"Spawn two normal scout bandits adjacent to self at the beginning of the round",
+"Replace your highest-level card with a level 1 or x card",
+"Replace the 2x modifier card in your deck with a curse",
+"Player gains disadvantage on all attacks for the first five rounds",
+"Player may not long rest during the scenario",
+"Enter the game at the beginning of round two",
+"Increase initiative by 10 for every round",
+"spent items are now consumed items",
+"Player can take no hand item cards into the scenario",
+"Player can take no head item cards into the scenario",
+"Player can take no chest item cards into the scenario",
+"Player can take no feet item cards into the scenario",
+"Player must take one less small item card into the scenario",
+"Make one normal enemy elite");
+
+
+function randomEffects() {
+  var randomPositive = positiveEffectsArray[Math.floor(Math.random()*positiveEffectsArray.length)];
+  var randomNegative = negativeEffectsArray[Math.floor(Math.random()*negativeEffectsArray.length)];
+  document.getElementById('positiveEffect').innerHTML = randomPositive;
+  document.getElementById('negativeEffect').innerHTML = randomNegative;
+}
