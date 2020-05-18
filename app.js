667ce73677c6919e00c2821abbcdbee7d08d5f5f
Tyler <tylerdburrell@gmail.com>
2020-05-17 21:47:27 -0500

    Effects list page implemented successfully

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-tyler:"+ process.env.DB_PASS +"@cluster0-h3nyl.gcp.mongodb.net/GloomDB", {
+mongoose.connect("mongodb+srv://admin-tyler:"+ process.env.DB_PASS +"@cluster0-h3nyl.gcp.mongodb.net/gloomDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const Schema = mongoose.Schema;
-
-const effectSchema = new Schema({
+const effectsSchema = {
   type: String,
   content: String
-});
+};
+
+const Effect = mongoose.model("Effect", effectsSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -34,13 +34,31 @@ app.get("/free-roll", function(req, res){
 });
 
 app.get("/effects-list", function(req, res){
-  res.render("effects-list");
+
+  Effect.find({}, function(err, foundEffects){
+    if(foundEffects.length === 0){
+      console.log("no items");
+      res.redirect("/");
+    }else {
+      console.log("success");
+
+      res.render("effects-list", {
+        effectItems: foundEffects
+      });
+    }
+
+  });
 });
 
 app.get("/hidden/post/update-effects", function(req, res){
   res.render("update-effects");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started");
 });
