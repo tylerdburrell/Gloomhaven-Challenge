13aa4155d2507d8951fe946f46cf4798998d1a5b
Tyler <tylerdburrell@gmail.com>
2020-06-19 08:43:58 -0500

    only require dotenv when not in production

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //jshint esversion:6
-require('dotenv').config();
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
